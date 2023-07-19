e1177ea42c27ff76b2a7e9f6f6ea5449e8860b6c
Natasha <75773163+natty-light@users.noreply.github.com>
2023-07-18 23:54:00 -0400

    yuh 2

--- main.go
+++ main.go
@@ -24,20 +24,21 @@ func main() {
 		fmt.Println("S3 Client config error", err)
 		return
 	}
-	d := utils.S3DataSource{Client: client}
-	// err = d.CheckTimeStamp(env)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
 
-	next, err := d.DownloadFile(env, NEXT_DAY_MESSAGE)
+	d := utils.S3DataSource{Client: client}
+	err = d.CheckTimeStamp(env)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	data := discordgo.MessageSend{Content: fmt.Sprintf("<@%s> %s", env.RoseUserId, next), AllowedMentions: &discordgo.MessageAllowedMentions{Users: []string{env.RoseUserId}}}
+	today, err := d.DownloadFile(env, NEXT_DAY_MESSAGE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	data := discordgo.MessageSend{Content: fmt.Sprintf("<@%s> %s", env.RoseUserId, today), AllowedMentions: &discordgo.MessageAllowedMentions{Users: []string{env.RoseUserId}}}
 
 	res, err := s.ChannelMessageSendComplex(env.ChannelId, &data)
 	if err != nil {
@@ -48,9 +49,10 @@ func main() {
 		if err != nil {
 			fmt.Println("Upload error: ", err)
 		} else {
-			if next == "left" {
+			// Upload opposite of what we sent today
+			if today == "left" {
 				err = d.UploadFile(env, "right", NEXT_DAY_MESSAGE)
-			} else if next == "right" {
+			} else if today == "right" {
 				err = d.UploadFile(env, "left", NEXT_DAY_MESSAGE)
 			}
 			if err != nil {
