96be86783ad88dffc06b6ad2fc277542003d395b
Natasha <75773163+natty-light@users.noreply.github.com>
2026-01-09 08:47:06 -0500

    regions

--- uploadLambda.sh
+++ uploadLambda.sh
@@ -1,3 +1,3 @@
 GOOS=linux GOARCH=amd64 go build -o bootstrap main.go
 zip bootstrap.zip bootstrap
-aws lambda update-function-code --function-name rose-gel-reminder --zip-file fileb://bootstrap.zip --profile default --region us-east-2
+aws lambda update-function-code --function-name rose-gel-reminder --zip-file fileb://bootstrap.zip --profile default --region us-east-1
